e5d26aad01e3af0492c41e7ed5731db99c166935
anskesh <laim207@mail.ru>
2021-01-08 20:34:24 +0300

    add 2 new fragments

--- app/src/main/java/com/efimcompany/mypsychologist/MainActivity.kt
+++ app/src/main/java/com/efimcompany/mypsychologist/MainActivity.kt
@@ -3,16 +3,24 @@ package com.efimcompany.mypsychologist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.efimcompany.mypsychologist.feature.messages.ui.MessangerFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initFunc()
+
         val fragmentManager = supportFragmentManager
+
 
         fragmentManager.beginTransaction()
             .add(R.id.container, MessangerFragment())
             .commit()
+    }
+
+    private fun initFunc() {
+        but
     }
 }
